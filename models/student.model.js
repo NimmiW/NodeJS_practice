5570c4408e6f94f79c543bf998aa51b469959ed1
Nimmi Rashinika <nimmirashinika@gmail.com>
2017-05-24 22:57:56 +0530

    -class

---
+++ models/student.model.js
@@ -0,0 +1,11 @@
+class Student {
+    studentname;
+    studentLocation;
+
+    constructor(name,location){
+        this.studentname = name;
+        this.studentLocation = location;
+    }
+}
+
+module.exports = Student;
--- server.js
+++ server.js
@@ -2,34 +2,33 @@ var express = require('express');
 var app = express();
 var sql = require("mssql");
 let studentController = require("./controllers/student.ctrl");
+let Student = require("./models/student.model");
 
-    // config for your database
-    var config = {
-        user: 'sa',
-        password: 'snklions',
-        server: 'localhost', 
-        database: 'SchoolDB' 
-    };
+// config for your database
+var config = {
+    user: 'sa',
+    password: 'snklions',
+    server: 'localhost',
+    database: 'SchoolDB'
+};
 
 app.get('student/', function (req, res) {
-   
+
     sql.connect(config, function (err) {
         if (err) console.log(err);
         var request = new sql.Request();
         request.query('select * from Student', function (err, recordset) {
             if (err) console.log(err)
             res.send(recordset);
-            sql.close();    
+            sql.close();
         });
     });
 });
 
 app.get('student/create', function (req, res) {
 
-    let student = {
-        studentname: req.query.studentname,
-        studentLocation: req.query.studentLocation
-    }
+    let student = new Student(req.query.studentname,req.query.studentLocation);
+
     response = studentController.create(student);
 
     res.send(response)
